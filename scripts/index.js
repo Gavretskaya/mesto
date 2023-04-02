cecf6ba1ea814d14439c9bfffcabc210cd61ab84
Anastasia Gavretskaya <gavretskaya@yandex.ru>
2023-04-02 23:32:14 +0300

    fixed 1 review

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,33 @@ const editLink = document.querySelector(".profile__edit-button");
 const editPopup = document.querySelector(".popup_type_edit");
 const editPopupCloseButton = editPopup.querySelector(".popup__close");
 const nameInput = editPopup.querySelector(".popup__input_type_name");
-const editPopupSave =  editPopup.querySelector(".popup__save");
 const editPopupForm = editPopup.querySelector(".popup__form");
 const editPopupChangeName = document.querySelector(".profile__title");
 const infoInput = editPopup.querySelector(".popup__input_type_info");
 const editPopupChangeInfo = document.querySelector(".profile__subtitle");
 
+editLink.addEventListener("click", () => {
+  editPopup.classList.add("popup_opened");
+});
 
 editLink.addEventListener("click", () => {
-  console.log("click");
-
-  editPopup.classList.add("popup_open");
-  nameInput.value = editPopupChangeName.innerHTML;
-  infoInput.value = editPopupChangeInfo.innerHTML;
+  nameInput.value = editPopupChangeName.textContent;
+  infoInput.value = editPopupChangeInfo.textContent;
 });
 
 editPopupCloseButton.addEventListener("click", () => {
-  editPopup.classList.remove("popup_open");
+  editPopup.classList.remove("popup_opened");
 });
 
 editPopupForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const name = nameInput.value;
   const info = infoInput.value;
-  editPopupChangeName.innerHTML = name;
-  editPopupChangeInfo.innerHTML = info;
-  editPopup.classList.remove("popup_open");
+  editPopupChangeName.textContent = name;
+  editPopupChangeInfo.textContent = info;
+  editPopup.classList.remove("popup_opened");
 });
 
 
 
-console.log("editlink");
-console.log("editPopup");
 
-console.log("JS")
