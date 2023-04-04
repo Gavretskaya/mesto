d8847efd667a16a965152d75fc820e16c9b03238
Anastasia Gavretskaya <gavretskaya@yandex.ru>
2023-04-04 21:46:44 +0300

    project mesto

--- scripts/index.js
+++ scripts/index.js
@@ -7,29 +7,29 @@ const editPopupChangeName = document.querySelector(".profile__title");
 const infoInput = editPopup.querySelector(".popup__input_type_info");
 const editPopupChangeInfo = document.querySelector(".profile__subtitle");
 
-function openPopup(popupName) {
+function openPopup() {
   editPopup.classList.add("popup_opened");
 };
 
-function closePopup(popupName) {
+function closePopup() {
   editPopup.classList.remove("popup_opened");
 };
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  closePopup (editPopup);
+  closePopup ();
   editPopupChangeName.textContent = nameInput.value;
   editPopupChangeInfo.textContent = infoInput.value;
 };
 
 editLink.addEventListener("click", () => {
-  openPopup(editPopup);
+  openPopup();
   nameInput.value = editPopupChangeName.textContent;
   infoInput.value = editPopupChangeInfo.textContent;
 });
 
 editPopupCloseButton.addEventListener("click", () => {
-  closePopup(editPopup);
+  closePopup();
 });
 
 editPopupForm.addEventListener('submit', formSubmitHandler)
