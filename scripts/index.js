9349eed6cb4dd465706c6581bfb4ba9dc13330f4
Anastasia Gavretskaya <gavretskaya@yandex.ru>
2023-04-11 21:04:44 +0300

    proba

--- scripts/index.js
+++ scripts/index.js
@@ -36,8 +36,34 @@ editPopupForm.addEventListener('submit', formSubmitHandler)
 
 
 
+const addLink = document.querySelector(".profile__add-button");
+const addPopup = document.querySelector(".popup_type_add");
+const addPopupCloseButton = addPopup.querySelector(".popup__close_card");
+const cardNameInput = addPopup.querySelector(".popup__input_type_card-name");
+const addPopupForm = addPopup.querySelector(".popup__add-form");
+const addPopupChangeNameCard = document.querySelector(".card__title");
+const cardURLInput = addPopup.querySelector(".popup popup_type_figure");
+const addPopupChangeImg = document.querySelector(".card__img");
 
+function openCardPopup() {
+  addPopup.classList.add("popup_opened_card");
+};
 
+function closeCardPopup() {
+  editPopup.classList.remove("popup_opened_card");
+};
+
+editLink.addEventListener("click", () => {
+  openCardPopup();
+  cardNameInput.value = addPopupChangeNameCard.textContent;
+  cardInput.value = addPopupChangeImg.textContent;
+});
+
+addPopupCloseButton.addEventListener("click", () => {
+  closeCardPopup();
+});
+
+addPopupForm.addEventListener('submit', formSubmitHandler)
 
 
 
